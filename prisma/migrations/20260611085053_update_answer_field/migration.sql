6e1c52b7aa7648a5705dde83b107b47c265aa66c
Notsurei <115835703+Notsurei@users.noreply.github.com>
2026-09-09 11:05:05 +0700

    Delete prisma/migrations directory

--- prisma/migrations/20260611085053_update_answer_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "PracticeAnswer" ALTER COLUMN "isCorrect" DROP NOT NULL;
-
--- AlterTable
-ALTER TABLE "UserAnswer" ALTER COLUMN "isCorrect" DROP NOT NULL;
--- prisma/migrations/20260618051124_add_email_verification/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "verifyToken" TEXT,
-ADD COLUMN     "verifyTokenExpiry" TIMESTAMP(3);
